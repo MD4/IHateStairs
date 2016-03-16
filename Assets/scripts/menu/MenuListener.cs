391a697c969f8a7e3e29ace1f962e24cd9780201
FXHibon <hibonfx@gmail.com>
2016-03-16 15:40:18 +0100

    Merge branch 'master' of /home/fx/StudioProjects/IHateStairs with conflicts.

---
+++ Assets/scripts/menu/MenuListener.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuListener : MonoBehaviour
+{
+  public void OnClick(string name)
+  {
+    Debug.Log(name + " clicked");
+    switch (name)
+    {
+      case "PLAY":
+        Application.LoadLevel("main");
+        break;
+      case "OPTIONS":
+        break;
+      case "EXIT":
+        Application.Quit();
+        break;
+    }
+
+  }
+}
