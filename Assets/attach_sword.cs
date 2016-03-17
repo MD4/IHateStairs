98c82c2e451438c39848d8b37a419f9d15b23f1d
Martin DEQUATREMARE <contact@martin-dequatremare.fr>
2016-03-17 13:37:53 +0100

    Added: Sword attached to hand

---
+++ Assets/attach_sword.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class attach_sword : MonoBehaviour {
+
+    public GameObject sword;
+    public GameObject bone;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        sword.transform.parent = bone.transform;
+        sword.transform.localPosition = Vector3.zero;
+        sword.transform.localRotation = Quaternion.identity;
+        //var someTransform = sword.transform;
+        //sword.parent = bone;
+        //sword.localPosition = Vector3.zero;
+        //sword.localRotation = Quaternion.identity;
+        //sword.localScale = Vector3.one;
+    }
+}
