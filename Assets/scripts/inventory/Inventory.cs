ccfb5be2136b3f1e93f521a117e035e05055498d
FXHibon <hibonfx@gmail.com>
2016-03-18 10:58:28 +0100

    Add inventory

---
+++ Assets/scripts/inventory/Inventory.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour
+{
+  public List<GameObject> gameObjects = new List<GameObject>();
+  public List<InventoryItem> itemObjects = new List<InventoryItem>();
+  public InventoryDataBase DataBase;
+  public GameObject slots;
+  private int x = -83;
+  private int y = 29;
+
+  // Use this for initialization
+  void Start()
+  {
+    Debug.Log("DataBase.Length  " + DataBase.Items.Count);
+
+    for (var i = 0; i < 2; i++)
+    {
+      for (var j = 0; j < 4; j++)
+      {
+        GameObject gameObject = Instantiate(slots);
+        gameObjects.Add(gameObject);
+        gameObject.transform.SetParent(this.gameObject.transform);
+        gameObject.GetComponent<RectTransform>().localPosition = new Vector3(x, y, 0);
+        gameObject.name = "Slot " + i + "." + j;
+        x += 55;
+        if (j == 3)
+        {
+          x = -83;
+          y -= 55;
+        }
+      }
+    }
+
+    AddItem(0);
+  }
+
+  void AddItem(int id)
+  {
+    if (itemObjects.Count < 8)
+    {
+      var tmp = DataBase.Items.Find(item => item.ItemId == id);
+      if (tmp != null)
+      {
+        itemObjects.Add(tmp);
+        gameObjects[id].GetComponent<ItemController>().Item = tmp;
+      }
+    }
+  }
+}
---
+++ Assets/scripts/inventory/InventoryItem.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class InventoryItem
+{
+  public int ItemId { get; set; }
+  public string Name { get; set; }
+  public string Desc { get; set; }
+  public ItemType Type { get; set; }
+  public Sprite ItemIcon { get; set; }
+  public GameObject modelObject;
+
+  public enum ItemType
+  {
+    Weapon,
+    Consumable,
+    Other
+  }
+
+  public InventoryItem(int itemId, string name, string desc, ItemType type)
+  {
+    ItemId = itemId;
+    Name = name;
+    Desc = desc;
+    Type = type;
+    ItemIcon  = Resources.Load<Sprite>("" + name);
+  }
+
+  public InventoryItem()
+  {
+  }
+}
