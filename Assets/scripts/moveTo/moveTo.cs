fb03263f22ef2bbe2cf3ec9e663cc1a46c6f408f
Baptiste Pignol <pignol.baptiste@gmail.com>
2016-03-18 15:55:16 +0100

    minotaure

--- Assets/scripts/moveTo/moveTo.cs
+++ Assets/scripts/moveTo/moveTo.cs
@@ -6,10 +6,12 @@ public class moveTo : MonoBehaviour {
 	public Transform goal;
 	public Animator animator;
 	public float attackDistance;
+	public float agroDistance;
 
 	private NavMeshAgent agent;
 	private EnemyAttack attack;
 	private EnemyHealth health;
+	private bool aggro = false;
 
 	void Start () {
 	  animator = GetComponent<Animator>();
@@ -19,17 +21,24 @@ public class moveTo : MonoBehaviour {
     }
 
     void Update() {
-       	agent.destination = goal.position;
-		animator.SetBool("walk", true);
-		float distance = Vector3.Distance(transform.position, goal.transform.position);
-		if (distance > attackDistance && !(health.currentHealth <= 0)) {
+    	float distance = Vector3.Distance(transform.position, goal.transform.position);
+    	if (agroDistance > distance) {
+    		aggro = true;
+    	}
+    	if (aggro) {
+    		agent.destination = goal.position;
 			animator.SetBool("walk", true);
-			attack.playerInRange = false;
-		} else {
-			animator.SetBool("walk", false);
-			agent.destination = transform.position;
-			attack.playerInRange = true;
+				if (distance > attackDistance && !(health.currentHealth <= 0)) {
+				animator.SetBool("walk", true);
+				attack.playerInRange = false;
+			} else {
+				animator.SetBool("walk", false);
+				agent.destination = transform.position;
+				attack.playerInRange = true;
 
-		}
+			}
+    	} else {
+    		agent.destination = transform.position;
+    	}
     }
 }
