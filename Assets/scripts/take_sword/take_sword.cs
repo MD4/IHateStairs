4fad09166d03ca31c81764e9dc1532d26e092986
Martin DEQUATREMARE <contact@martin-dequatremare.fr>
2016-03-18 12:06:17 +0100

    Yeah ! He takes his sword !

---
+++ Assets/scripts/take_sword/take_sword.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class take_sword : MonoBehaviour {
+
+    public GameObject helmet;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "helmet")
+        {
+            Debug.Log("take sword");
+            Destroy(this.gameObject);
+            helmet.GetComponent<attach_sword>().hasSword = true;
+        }
+    }
+
+}
